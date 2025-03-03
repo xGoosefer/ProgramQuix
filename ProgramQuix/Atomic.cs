106bb265a3dac3ee4bfb9ad642568b935d6ef038
Roberto Ramirez <robaram726@gmail.com>
2025-03-03 13:17:20 -0600

    Changed to animal databes

--- ProgramQuix/Atomic.cs
+++ ProgramQuix/Atomic.cs
@@ -3,23 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ProgramQuix
 {
     internal class Atomic
     {
         // This is a property of the class, hence its position
-        Dictionary<string, int> atomicweights = new Dictionary<string, int>()
+
+        Dictionary<string, string> names = new Dictionary<string, string>()
         {
-            {"hydrogen", 1 },
-            {"helium", 2},
-            {"lithium", 3},
-            {"beryllium", 4}
+            {"hippo", "Hippopotamus Amphibius"},
+            {"bear", "Euarctos Americanus"},
+            {"chipmunk", "Eutamias"},
+            {"gopher", "Thomomys"},
+            {"muskrat", "Ondatra Zibethica"}
         };
+
+        private string Find(string item)
+        {
+            if (names.ContainsKey(item))
+            {
+                return names[item];
+            }
+            return "Sorry, that animal was not found in our database.";
+        }
 
         public void Start()
         {
-            Console.WriteLine($"Our database has {atomicweights.Count} records.");
+            Clear();
+            WriteLine("Enter an animal name (example: hippo)");
+            string input = ReadLine();
+            WriteLine(Find(input));
+            WriteLine("Press x to exit, any other key to enter another request...");
+            input = ReadLine();
+            if (input.ToLower() == "x")
+            {
+                //forces window to close, but not needed here
+                //Environment.Exit(0);
+            }
+            else
+            {
+                Start();
+            }
         }
     }
 }
