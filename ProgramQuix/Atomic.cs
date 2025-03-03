8789f44d98700a4c9338d2a1a369bb3717e6ff38
Roberto Ramirez <robaram726@gmail.com>
2025-03-03 13:10:01 -0600

    Print record amoutn

--- ProgramQuix/Atomic.cs
+++ ProgramQuix/Atomic.cs
@@ -8,9 +8,18 @@ namespace ProgramQuix
 {
     internal class Atomic
     {
+        // This is a property of the class, hence its position
+        Dictionary<string, int> atomicweights = new Dictionary<string, int>()
+        {
+            {"hydrogen", 1 },
+            {"helium", 2},
+            {"lithium", 3},
+            {"beryllium", 4}
+        };
+
         public void Start()
         {
-
+            Console.WriteLine($"Our database has {atomicweights.Count} records.");
         }
     }
 }
